f6eb67275b788225330be234ddf8ae86f39fdcfb
GABRIEL DE FARIAS OLIVEIRA <gabrielfoliveira2020@gmail.com>
2021-05-20 11:40:13 -0300

    20/05/2021 = fim do add e inicio do listar

--- src/app/add-usuario/add-usuario.page.ts
+++ src/app/add-usuario/add-usuario.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { promise } from 'protractor';
+import { PostService } from './../../services/post.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-usuario',
@@ -6,27 +9,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-usuario.page.scss'],
 })
 export class AddUsuarioPage implements OnInit {
-  id: number=0;
+  id: string="";
   nome: string="";
   email: string="";
   telefone: string="";
   cidade: string="";
   cpf: string="";
 
-  constructor() { }
+  constructor(
+    private service:PostService,
+    private router:Router,
+    private actRoute:ActivatedRoute
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.actRoute.params.subscribe((dadosdarota:any)=>{
+      this.id = dadosdarota.id;
+      this.nome = dadosdarota.nome;
+      this.email = dadosdarota.email;
+      this.telefone = dadosdarota.telefone;
+      this.cidade = dadosdarota.cidade;
+      this.cpf = dadosdarota.cpf;
+    });
+  }
 
   cadastrar() {
-    let usuario = {
-      id:this.id,
-      nome:this.nome,
-      email:this.email,
-      telefone:this.telefone,
-      cidade:this.cidade,
-      cpf:this.cpf
-    };
-    console.log(usuario);
+    return new Promise(resolve => {
+      let usuario = {
+        requisicao: 'add',
+        nome:this.nome,
+        email:this.email,
+        telefone:this.telefone,
+        cidade:this.cidade,
+        cpf:this.cpf
+      };
+      console.log(usuario);
+      this.service.dadosApi(usuario, 'api.php').subscribe(data => {
+        if(data['success']) {
+          this.router.navigate(['lista-usuarios']);
+          console.log(data);
+          this.id = "";
+          this.nome = "";
+          this.email = "";
+          this.telefone = "";
+          this.cidade = "";
+          this.cpf = "";
+        }
+      });
+    });
   }
 
 }
--- src/app/lista-usuarios/lista-usuarios.page.ts
+++ src/app/lista-usuarios/lista-usuarios.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lista-usuarios',
@@ -7,11 +7,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./lista-usuarios.page.scss'],
 })
 export class ListaUsuariosPage implements OnInit {
+  id: string="";
+  nome: string="";
+  email: string="";
+  telefone: string="";
+  cidade: string="";
+  cpf: string="";
 
-  constructor(private router:Router) { }
+  constructor(
+    private router:Router,
+    private actRoute:ActivatedRoute
+  ) { }
 
-  ngOnInit() {}
-  
+  ngOnInit() {
+    this.actRoute.params.subscribe((dadosdarota:any)=>{
+      this.id = dadosdarota.id;
+      this.nome = dadosdarota.nome;
+      this.email = dadosdarota.email;
+      this.telefone = dadosdarota.telefone;
+      this.cidade = dadosdarota.cidade;
+      this.cpf = dadosdarota.cpf;
+    });
+  }
+
   addUsuario() {
     this.router.navigate(["add-usuario"]);
   }
